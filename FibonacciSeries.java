66fe7571e341884a2e50820bd71d47d5c98d392e
SREEhari11-ai <23124051@srcas.ac.in>
2025-03-14 15:19:33 +0530

    Update BubbleSortExample.java

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -1,20 +1,40 @@
-import java.util.Scanner;
+public class BubbleSortExample {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
 
-public class FibonacciSeries {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the number of terms: ");
-        int n = scanner.nextInt();
-        scanner.close();
-
-        int first = 0, second = 1;
-        System.out.print("Fibonacci Series: " + first + " " + second);
-
-        for (int i = 2; i < n; i++) {
-            int next = first + second;
-            System.out.print(" " + next);
-            first = second;
-            second = next;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+          
+            if (!swapped) break;
         }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Original array:");
+        printArray(array);
+
+        bubbleSort(array);
+
+        System.out.println("Sorted array:");
+        printArray(array);
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
     }
 }
